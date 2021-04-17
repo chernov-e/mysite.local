e7d5fa3d178bf4e794506a47c6646158891dc812
chernov <evg.tchernov@gmail.com>
2021-04-17 21:34:18 +0300

    simple site structure

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
-<?php
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <header>
+      <h1><?php echo '<p>Hello, <b>NIX Education</b></p>'; ?></h1>
+    </header>
+    <nav>
+      <a href="/1.php">урок 1</a> | <a href="/2.php">урок 2</a>
+    </nav>
+    <main>
 
-echo '<p>Hello, <b>NIX Education</b></p>';
+    </main>
+    <footer>© 2021 mysite.local</footer>
+  </body>
+</html>
